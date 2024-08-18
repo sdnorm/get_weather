188b28f5d20ca94da9ea015e7102e9bafa301e6d
Spencer Norman <spencerdnorman@gmail.com>
2024-08-18 15:32:45 -0500

    removed a not needed route and removed a method from the weather forecasts controller that isn't needed

--- test/controllers/weather_forecasts_controller_test.rb
+++ test/controllers/weather_forecasts_controller_test.rb
@@ -1,13 +1,8 @@
 require "test_helper"
 
 class WeatherForecastsControllerTest < ActionDispatch::IntegrationTest
-  test "should get current" do
-    get weather_forecasts_current_url
-    assert_response :success
-  end
-
   test "should get extended" do
-    get weather_forecasts_extended_url
+    get extended_weather_forecast_url
     assert_response :success
   end
 end
