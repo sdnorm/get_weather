8f35111c643838f2a662507dcbeaa6b5b28d3a75
Spencer Norman <spencerdnorman@gmail.com>
2024-08-18 15:48:33 -0500

    added weather url and creds, added httparty gem

--- app/models/weather_service.rb
+++ app/models/weather_service.rb
@@ -1,2 +1,4 @@
 class WeatherService < ApplicationRecord
+  CURRENT_WEATHER_URL = "https://api.tomorrow.io/v4/weather/forecast"
+  # ?location=70769&apikey=#{Rails.application.credentials.tomorrow_io_weather[:key]}"
 end
