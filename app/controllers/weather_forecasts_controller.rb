a5f9cd32003d50e04b83dc42a21c9365851b5ea5
Spencer Norman <spencerdnorman@gmail.com>
2024-08-19 01:45:01 -0500

    getting error messages dispalying in the UI

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -7,6 +7,11 @@ class WeatherForecastsController < ApplicationController
       WeatherService.new(@zip_code, "current").get_forecast
     end
 
+    if @todays_weather.is_a?(Hash) && @todays_weather.key?(:error)
+      @error_message = @todays_weather[:error]
+      @todays_weather = nil
+    end
+
     respond_to do |format|
       format.html
       format.turbo_stream { render "weather_forecasts/todays_weather" }
@@ -19,6 +24,11 @@ class WeatherForecastsController < ApplicationController
     @is_cached = Rails.cache.exist?(cache_key)
     @extended_forecast = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
       WeatherService.new(@zip_code, "extended").get_forecast
+    end
+
+    if @extended_forecast.is_a?(Hash) && @extended_forecast.key?(:error)
+      @error_message = @extended_forecast[:error]
+      @extended_forecast = nil
     end
 
     respond_to do |format|
--- app/models/weather_service.rb
+++ app/models/weather_service.rb
@@ -41,10 +41,8 @@ class WeatherService
     if response.success?
       response.parsed_response
     else
-      if response.code == 429
-        return { error: "Rate limit exceeded. Please try again later." }
-      end
-      { error: response.parsed_response["error"] || "An error occurred" }
+      error_message = parse_error_message(response)
+      { error: error_message }
     end
   rescue JSON::ParserError
     { error: "Invalid response format" }
@@ -52,28 +50,51 @@ class WeatherService
     { error: "HTTP request failed: #{e.message}" }
   end
 
+  def parse_error_message(response)
+    if response.code == 429
+      "Rate limit exceeded. Please try again later."
+    elsif response.parsed_response.is_a?(Hash) && response.parsed_response["message"]
+      response.parsed_response["message"]
+    else
+      "An error occurred: #{response.code}"
+    end
+  end
+
   def parse_current_weather(data)
+    return { error: data[:error] } if data.is_a?(Hash) && data.key?(:error)
+
     minutely_data = data.dig("timelines", "minutely", 0, "values")
     daily_data = data.dig("timelines", "daily", 0)
     location_data = data.dig("location")
+
+    if minutely_data.nil? || daily_data.nil? || location_data.nil?
+      return { error: "Unable to retrieve weather data. Please check your zip code and try again." }
+    end
+
     OpenStruct.new(
-      current_temp: minutely_data&.dig("temperature").round,
-      high_temp: daily_data&.dig("values", "temperatureMax").round,
-      low_temp: daily_data&.dig("values", "temperatureMin").round,
-      location: location_data&.dig("name"),
-      date: daily_data&.dig("time").to_date.strftime("%A, %B %d")
+      current_temp: minutely_data.dig("temperature")&.round,
+      high_temp: daily_data.dig("values", "temperatureMax")&.round,
+      low_temp: daily_data.dig("values", "temperatureMin")&.round,
+      location: location_data.dig("name"),
+      date: daily_data.dig("time")&.to_date&.strftime("%A, %B %d")
     )
   end
 
   def parse_extended_forecast(data)
-    extended_forecast = []
-    data.dig("timelines", "daily").map do |day|
-      extended_forecast << OpenStruct.new(
-        date: day["time"].to_date.strftime("%A, %B %d"),
-        high_temp: day["values"]["temperatureMax"].round,
-        low_temp: day["values"]["temperatureMin"].round
+    return { error: data[:error] } if data.is_a?(Hash) && data.key?(:error)
+
+    daily_data = data.dig("timelines", "daily")
+    
+    if daily_data.nil? || daily_data.empty?
+      return { error: "Unable to retrieve extended forecast. Please check your zip code and try again." }
+    end
+
+    daily_data.map do |day|
+      OpenStruct.new(
+        date: day["time"]&.to_date&.strftime("%A, %B %d"),
+        high_temp: day.dig("values", "temperatureMax")&.round,
+        low_temp: day.dig("values", "temperatureMin")&.round
       )
     end
-    extended_forecast
   end
 end
